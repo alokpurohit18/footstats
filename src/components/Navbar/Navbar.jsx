614135d832327458914b74633801ab9ab0694ae6
alokpurohit18 <alokpurohit18@gmail.com>
2022-01-02 12:17:11 +0530

    basic styling added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,37 +3,40 @@ import Avatar from "@mui/material/Avatar";
 import "antd/dist/antd.css";
 import { appLogo, appName } from "../../variables";
 import Search from "../Search/Search.jsx";
-import Menu from "../Menu/Menu.jsx";
 import "./Navbar.scss";
 
 class Navbar extends React.Component {
   pages;
-  settings;
 
   constructor(props) {
     super(props);
     this.pages = this.props.pages;
-    this.settings = this.props.settings;
   }
 
   createNavElement = (pageName) => {
-    return <div className="nav-element ant-col">{pageName}</div>;
+    return (
+      <div className="nav-element ant-col">
+        <a href="in">{pageName}</a>
+      </div>
+    );
   };
 
   render() {
     return (
       <div className="navbar-main ant-row">
-        <div className="app-logo-container ant-col ant-col-2">
-          <Avatar alt="App Logo" src={appLogo} />
+        <div className="app-logo-container ant-col ant-col-1">
+          <span className="app-logo">
+            <Avatar alt="App Logo" src={appLogo} />
+          </span>
         </div>
 
         <div className="app-name-container ant-col ant-col-4">
-          Welcome to {appName}!!!
+          <a href="in">Welcome to {appName}!!!</a>
         </div>
 
         {this.pages.map(this.createNavElement)}
 
-        <Search placeholder="Search"/>
+        <Search placeholder="Search" searchIcon="🔍" />
       </div>
     );
   }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,26 +1,29 @@
 import * as React from "react";
 import "antd/dist/antd.css";
-import { appLogo, appName } from "../../variables";
+import "./Search.scss";
 
 class Search extends React.Component {
   placeholder;
-  settings;
+  searchIcon;
 
   constructor(props) {
     super(props);
     this.placeholder = this.props.placeholder;
-    this.settings = this.props.settings;
+    this.searchIcon = this.props.searchIcon;
   }
+
+  handleSearch = () => {};
 
   render() {
     return (
       <div className="search-container ant-row">
         <input
-          className="search-bar ant-col ant-col-8"
+          className="search-bar ant-col ant-col-24"
           type="search"
           placeholder={this.placeholder}
+          onChange={this.handleSearch}
         />
-        <div className="search-icon ant-col ant-col-2">🔍</div>
+        <div className="search-icon ant-col ant-col-4">{this.searchIcon}</div>
       </div>
     );
   }
