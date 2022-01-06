818eddf0a266371a8097f4e79c4b70aba13637d7
alokpurohit18 <alokpurohit18@gmail.com>
2022-01-06 22:29:33 +0530

    web scraping successfully started

--- src/test.py
+++ src/test.py
@@ -1,14 +1,55 @@
 #!C:\Python39\python.exe
-import numpy as np
-L=2
-matrix = np.arange((L*L)/1).reshape((L,L))
-rows, cols = matrix.shape
-dictionary = [{'x':j, 'y':i, 'value':matrix[j,i]} for j in range(rows) for i in range(cols)]
+import requests
+from bs4 import BeautifulSoup
 
-import json
-import sys
+news_url = "https://www.skysports.com/football/news"
+scores_url = "https://www.espn.in/football/scoreboard/_/league/all/"
+stats_url = ""
 
-sys.stdout = open('./test_data.js', 'w')
+def create_news_data(url):
+    request = requests.get(url)
+    soup = BeautifulSoup(request.content, "html.parser")
+    news_source = soup.find_all('a')
 
-jsonobj = json.dumps(dictionary)
-print("var jsonstr = {} ".format(jsonobj))
+    news_array = []
+    for news_object in news_source:
+        if(news_object.has_attr('aria-label')):
+            news_array.append(news_object)
+    
+    for news_object in news_array:
+        print(news_object['title'])
+        #print(news_object['href'])
+        #print(news_object.find('img')['data-src']
+        #print(news_object.find('img')['alt'])
+
+def create_scores_data(url):
+    request = requests.get(url)
+    soup = BeautifulSoup(request.content, "html.parser")
+    scores_source = soup.find('div', class_='team-info')
+    #team_name = scores_source.find_next('span', class_='short-name')
+    print(scores_source)
+    #for title_object in scores_source:
+        #print(title_object)
+
+    #scores_array = []
+    #for score_object in scores_source:
+        #if(score_object.has_attr('aria-label')):
+            #scores_array.append(score_object)
+    
+    #for news_object in scores_array:
+        #print(score_object['title'])
+        #print(news_object['href'])
+        #print(news_object.find('img')['data-src']
+        #print(news_object.find('img')['alt'])
+
+create_news_data(news_url)
+create_scores_data(scores_url)
+
+
+#import json
+#import sys
+
+#sys.stdout = open('test_data.js', 'w')
+
+#jsonobj = json.dumps(dictionary)
+#print("var jsonstr = {} ".format(jsonobj))
