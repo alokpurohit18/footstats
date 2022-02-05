887ea0ad48b98d2fd66a564d6f5f0ebdef8eed48
alokpurohit18 <alokpurohit18@gmail.com>
2022-02-05 19:59:39 +0530

    player tab part 2 complete

--- src/components/PlayersSection/PlayersSection.jsx
+++ src/components/PlayersSection/PlayersSection.jsx
@@ -30,7 +30,8 @@ class PlayersSection extends React.Component {
     if (value < 1000) {
       return value;
     } else if (value >= 1000 && value < 1000000) {
-      returnString = returnString.substring(0, 3) + "K";
+      value = value / 1000.0;
+      returnString = value.toString() + "K";
       return returnString;
     } else {
       value = value / 1000000.0;
@@ -55,16 +56,25 @@ class PlayersSection extends React.Component {
   render() {
     return (
       <div className="players-section-main">
-        <h2>L. Messi</h2>
+        <h2>
+          {Object.keys(this.state.player).length === 0 &&
+          this.state.player.constructor === Object
+            ? null
+            : this.state.player.personal_details.name.short_name}
+        </h2>
         {this.state.apiLoaded ? null : (
-          <LoadAPI url="/playerDetails" sourceLink={2} setData={this.setData} />
+          <LoadAPI
+            url="/playerDetails"
+            sourceLink={122}
+            setData={this.setData}
+          />
         )}
         {Object.keys(this.state.player).length === 0 &&
         this.state.player.constructor === Object ? null : (
           <div className="player-card-main ant-row">
             <div className="ant-col ant-col-6">
               <h2 className="sub-heading">All Overall Ratings</h2>
-              <div className="all-overall-rating "></div>
+              <div className="all-overall-rating"></div>
             </div>
             <div className="player-details ant-col ant-col-18">
               <div className="ant-row">
@@ -233,34 +243,278 @@ class PlayersSection extends React.Component {
                     {this.state.player.club.league.name}
                   </div>
                 </div>
-                <div className="nation-info ant-col">
-                  <div className="ant-row">
-                    <div className="ant-col">
+                {this.state.player.national_team.id === "" ? null : (
+                  <div className="nation-info ant-col">
+                    <div className="ant-row">
+                      <div className="ant-col">
+                        <img
+                          className="nation-flag-logo"
+                          alt="nation flag logo"
+                          src={this.state.player.national_team.flag_url}
+                        />
+                      </div>
+                      <div className="sub-heading ant-col">
+                        {this.state.player.national_team.name.toUpperCase()}
+                      </div>
+                    </div>
+                    <div className="ant-row">
                       <img
-                        className="nation-flag-logo"
-                        alt="nation flag logo"
-                        src={this.state.player.national_team.flag_url}
+                        className="club-logo"
+                        alt="club logo"
+                        src={this.state.player.national_team.logo_url}
                       />
                     </div>
-                    <div className="sub-heading ant-col">
-                      {this.state.player.national_team.name.toUpperCase()}
+                    <div>
+                      <span className="label">Position: </span>
+                      {this.state.player.national_team.position}
+                    </div>
+                    <div>
+                      <span className="label">Kit Number: </span>
+                      {this.state.player.national_team.jersey_number}
                     </div>
                   </div>
-                  <div className="ant-row">
-                    <img
-                      className="club-logo"
-                      alt="club logo"
-                      src={this.state.player.national_team.logo_url}
-                    />
+                )}
+              </div>
+            </div>
+            <div className="ant-col ant-col-6">
+              <h2 className="sub-heading">Analysis of Statistics</h2>
+              <div className="ratings-analysis"></div>
+            </div>
+            <div className="player-details ant-col ant-col-18">
+              <div className="ant-row">
+                <div className="pace ant-col ant-col-6">
+                  <h2 className="sub-heading">PACE</h2>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.movement.sprint_speed}
+                    </span>
+                    <span className="label"> Sprint Speed</span>
                   </div>
-                  <div>
-                    <span className="label">Position: </span>
-                    {this.state.player.national_team.position}
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.movement.acceleration}
+                    </span>
+                    <span className="label"> Acceleration</span>
                   </div>
-                  <div>
-                    <span className="label">Kit Number: </span>
-                    {this.state.player.national_team.jersey_number}
+                </div>
+                <div className="shooting ant-col ant-col-6">
+                  <h2 className="sub-heading">SHOOTING</h2>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.attacking.finishing}
+                    </span>
+                    <span className="label"> Finishing</span>
                   </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.defending.positioning}
+                    </span>
+                    <span className="label"> Positioning</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.attacking.shot_power}
+                    </span>
+                    <span className="label"> Shot Power</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.attacking.long_shots}
+                    </span>
+                    <span className="label"> Long Shots</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.skill.penalties}
+                    </span>
+                    <span className="label"> Penalties</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.attacking.volleys}
+                    </span>
+                    <span className="label"> Volleys</span>
+                  </div>
+                </div>
+                <div className="passing ant-col ant-col-6">
+                  <h2 className="sub-heading">PASSING</h2>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.attacking.finishing}
+                    </span>
+                    <span className="label"> Vision</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.attacking.crossing}
+                    </span>
+                    <span className="label"> Crossing</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.skill.fk_accuracy}
+                    </span>
+                    <span className="label"> FK Accuracy</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.attacking.long_passing}
+                    </span>
+                    <span className="label"> Long Passing</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.attacking.short_passing}
+                    </span>
+                    <span className="label"> Short Passing</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.skill.fk_curve}
+                    </span>
+                    <span className="label"> Curve</span>
+                  </div>
+                </div>
+                <div className="dribbling ant-col ant-col-6">
+                  <h2 className="sub-heading">DRIBBLING</h2>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.movement.agility}
+                    </span>
+                    <span className="label"> Agility</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.movement.balance}
+                    </span>
+                    <span className="label"> Balance</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.movement.reactions}
+                    </span>
+                    <span className="label"> Reactions</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.skill.composure}
+                    </span>
+                    <span className="label"> Composure</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.skill.ball_control}
+                    </span>
+                    <span className="label"> Ball Control</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.skill.dribbling}
+                    </span>
+                    <span className="label"> Dribbling</span>
+                  </div>
+                </div>
+              </div>
+              <div className="ant-row">
+                <div className="defending ant-col ant-col-6">
+                  <h2 className="sub-heading">DEFENDING</h2>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.defending.interceptions}
+                    </span>
+                    <span className="label"> Interceptions</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.attacking.heading_accuracy}
+                    </span>
+                    <span className="label"> Heading Accuracy</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.defending.marking_awareness}
+                    </span>
+                    <span className="label"> Defensive Awareness</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.defending.standing_tackle}
+                    </span>
+                    <span className="label"> Standing Tackle</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.defending.sliding_tackle}
+                    </span>
+                    <span className="label"> Sliding Tackle</span>
+                  </div>
+                </div>
+                <div className="physical ant-col ant-col-6">
+                  <h2 className="sub-heading">PHYSICAL</h2>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.generic.jumping}
+                    </span>
+                    <span className="label"> Jumping</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.generic.stamina}
+                    </span>
+                    <span className="label"> Stamina</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.generic.strength}
+                    </span>
+                    <span className="label"> Strength</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.defending.aggression}
+                    </span>
+                    <span className="label"> Aggression</span>
+                  </div>
+                </div>
+                <div className="goalkeeping ant-col ant-col-6">
+                  <h2 className="sub-heading">GOALKEEPING</h2>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.goalkeeping.diving}
+                    </span>
+                    <span className="label"> GK Diving</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.goalkeeping.handling}
+                    </span>
+                    <span className="label"> GK Handling</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.goalkeeping.kicking}
+                    </span>
+                    <span className="label"> GK Kicking</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.goalkeeping.positioning}
+                    </span>
+                    <span className="label"> GK Positioning</span>
+                  </div>
+                  <div className="stat-row">
+                    <span className="stat-box">
+                      {this.state.player.stats.goalkeeping.reflexes}
+                    </span>
+                    <span className="label"> GK Reflexes</span>
+                  </div>
+                </div>
+                <div className="traits ant-col ant-col-6">
+                  <h2 className="sub-heading">TRAITS</h2>
+                  {this.loadPlayerSpecialities(
+                    this.state.player.stats.generic.traits
+                  )}
+                  {this.abilityArray.map(this.createPlayerSpecialities)}
                 </div>
               </div>
             </div>
