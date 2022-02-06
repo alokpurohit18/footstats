6ca8a05a6295fb11221b2091a01220337c65dd0b
alokpurohit18 <alokpurohit18@gmail.com>
2022-02-06 13:16:21 +0530

    player stats tab complete

--- src/components/PlayersSection/PlayersSection.jsx
+++ src/components/PlayersSection/PlayersSection.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import "antd/dist/antd.css";
 import LoadAPI from "../../utils/LoadAPI";
 import "./PlayersSection.scss";
+import { soccerBallUrl } from "../../utils/variables.js";
 
 class PlayersSection extends React.Component {
   sectionHeading;
@@ -63,18 +64,191 @@ class PlayersSection extends React.Component {
             : this.state.player.personal_details.name.short_name}
         </h2>
         {this.state.apiLoaded ? null : (
-          <LoadAPI
-            url="/playerDetails"
-            sourceLink={122}
-            setData={this.setData}
-          />
+          <LoadAPI url="/playerDetails" sourceLink={0} setData={this.setData} />
         )}
         {Object.keys(this.state.player).length === 0 &&
         this.state.player.constructor === Object ? null : (
           <div className="player-card-main ant-row">
             <div className="ant-col ant-col-6">
               <h2 className="sub-heading">All Overall Ratings</h2>
-              <div className="all-overall-rating"></div>
+              <div className="all-overall-rating">
+                <div className="all-overall-container">
+                  <div className="ant-row">
+                    <div className="ant-row row-container">
+                      <div className="ant-col">
+                        <div>LS</div>
+                        <div>{this.state.player.stats.rating_as.attack.ls}</div>
+                      </div>
+                      <div className="ant-col">
+                        <div>ST</div>
+                        <div>{this.state.player.stats.rating_as.attack.st}</div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RS</div>
+                        <div>{this.state.player.stats.rating_as.attack.rs}</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ant-row">
+                    <div className="ant-row row-container">
+                      <div className="ant-col">
+                        <div>LW</div>
+                        <div>{this.state.player.stats.rating_as.attack.lw}</div>
+                      </div>
+                      <div className="ant-col">
+                        <div>LF</div>
+                        <div>{this.state.player.stats.rating_as.attack.lf}</div>
+                      </div>
+                      <div className="ant-col">
+                        <div>CF</div>
+                        <div>{this.state.player.stats.rating_as.attack.cf}</div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RF</div>
+                        <div>{this.state.player.stats.rating_as.attack.rf}</div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RW</div>
+                        <div>{this.state.player.stats.rating_as.attack.rw}</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ant-row">
+                    <div className="ant-row row-container">
+                      <div className="ant-col">
+                        <div>LAM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.lam}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>CAM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.cam}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RAM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.ram}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ant-row">
+                    <div className="ant-row row-container">
+                      <div className="ant-col">
+                        <div>LM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.lm}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>LCM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.lcm}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>CM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.cm}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RCM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.rcm}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.rm}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ant-row">
+                    <div className="ant-row row-container">
+                      <div className="ant-col">
+                        <div>LWB</div>
+                        <div>
+                          {this.state.player.stats.rating_as.defence.lwb}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>LDM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.ldm}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>CDM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.cdm}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RDM</div>
+                        <div>
+                          {this.state.player.stats.rating_as.midfield.rdm}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RWB</div>
+                        <div>
+                          {this.state.player.stats.rating_as.defence.rwb}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ant-row">
+                    <div className="ant-row row-container">
+                      <div className="ant-col">
+                        <div>LB</div>
+                        <div>
+                          {this.state.player.stats.rating_as.defence.lb}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>LCB</div>
+                        <div>
+                          {this.state.player.stats.rating_as.defence.lcb}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>CB</div>
+                        <div>
+                          {this.state.player.stats.rating_as.defence.cb}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RCB</div>
+                        <div>
+                          {this.state.player.stats.rating_as.defence.rcb}
+                        </div>
+                      </div>
+                      <div className="ant-col">
+                        <div>RB</div>
+                        <div>
+                          {this.state.player.stats.rating_as.defence.rb}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ant-row">
+                    <div className="ant-row row-container">
+                      <div className="ant-col">
+                        <div>GK</div>
+                        <div>
+                          {this.state.player.stats.rating_as.defence.gk}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="player-details ant-col ant-col-18">
               <div className="ant-row">
@@ -277,8 +451,40 @@ class PlayersSection extends React.Component {
               </div>
             </div>
             <div className="ant-col ant-col-6">
-              <h2 className="sub-heading">Analysis of Statistics</h2>
-              <div className="ratings-analysis"></div>
+              <div className="ratings-analysis-container">
+                <h2 className="sub-heading">Base Statistics</h2>
+                <div className="ratings-analysis">
+                  <img
+                    className="soccer-ball"
+                    src={soccerBallUrl}
+                    alt="football img"
+                  />
+                  <div className="base-stat pace">
+                    <span className="stat-box">
+                      {this.state.player.stats.generic.pace}
+                    </span>{" "}
+                    PAC
+                  </div>
+                  <div className="base-stat shooting">
+                    SHO{" "}
+                    <span className="stat-box">
+                      {this.state.player.stats.generic.pace}
+                    </span>
+                  </div>
+                  <div className="base-stat passing">
+                    PAS{" "}
+                    <span className="stat-box">
+                      {this.state.player.stats.generic.pace}
+                    </span>
+                  </div>
+                  <div className="base-stat defending">
+                    <span className="stat-box">
+                      {this.state.player.stats.generic.pace}
+                    </span>{" "}
+                    DEF
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="player-details ant-col ant-col-18">
               <div className="ant-row">
--- src/utils/variables.js
+++ src/utils/variables.js
@@ -2,3 +2,7 @@ export const appLogo =
   "https://pbs.twimg.com/profile_images/1381979354910064641/Y00T62Sm_400x400.jpg";
 export const appName = "FootStats";
 export const combinedLogo = "https://i.redd.it/em1s8xzjs8b81.jpg";
+export const soccerFieldUrl =
+  "https://media.istockphoto.com/vectors/realistic-vertical-football-soccer-field-illustration-vector-id499753566?k=20&m=499753566&s=170667a&w=0&h=DwpHNQxSBQuqvsNKKmb96Y5EOpvhGgiM6IiVNjkJFTk=";
+export const soccerBallUrl =
+  "https://www.soccerpro.com/wp-content/uploads/cq7147_710_nike_hi_vis_premier_league_flight_official_match_ball_yellow_laser_crimson_with_purple_01.jpg";
