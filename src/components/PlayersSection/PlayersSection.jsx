71f5dd22ca240f8d4a48688b0350362d97a6f8ff
alokpurohit18 <alokpurohit18@gmail.com>
2022-02-06 16:54:40 +0530

    player stats gk bug fix

--- src/components/PlayersSection/PlayersSection.jsx
+++ src/components/PlayersSection/PlayersSection.jsx
@@ -64,7 +64,11 @@ class PlayersSection extends React.Component {
             : this.state.player.personal_details.name.short_name}
         </h2>
         {this.state.apiLoaded ? null : (
-          <LoadAPI url="/playerDetails" sourceLink={0} setData={this.setData} />
+          <LoadAPI
+            url="/playerDetails"
+            sourceLink={6}
+            setData={this.setData}
+          />
         )}
         {Object.keys(this.state.player).length === 0 &&
         this.state.player.constructor === Object ? null : (
@@ -459,30 +463,61 @@ class PlayersSection extends React.Component {
                     src={soccerBallUrl}
                     alt="football img"
                   />
-                  <div className="base-stat pace">
-                    <span className="stat-box">
-                      {this.state.player.stats.generic.pace}
-                    </span>{" "}
-                    PAC
-                  </div>
-                  <div className="base-stat shooting">
-                    SHO{" "}
-                    <span className="stat-box">
-                      {this.state.player.stats.generic.pace}
-                    </span>
-                  </div>
-                  <div className="base-stat passing">
-                    PAS{" "}
-                    <span className="stat-box">
-                      {this.state.player.stats.generic.pace}
-                    </span>
-                  </div>
-                  <div className="base-stat defending">
-                    <span className="stat-box">
-                      {this.state.player.stats.generic.pace}
-                    </span>{" "}
-                    DEF
-                  </div>
+                  {this.state.player.personal_details.positions === "GK" ? (
+                    <div>
+                      <div className="base-stat pace">
+                        <span className="stat-box">
+                          {this.state.player.stats.goalkeeping.diving}
+                        </span>{" "}
+                        DIV
+                      </div>
+                      <div className="base-stat shooting">
+                        HAN{" "}
+                        <span className="stat-box">
+                          {this.state.player.stats.goalkeeping.handling}
+                        </span>
+                      </div>
+                      <div className="base-stat passing">
+                        KIC{" "}
+                        <span className="stat-box">
+                          {this.state.player.stats.goalkeeping.kicking}
+                        </span>
+                      </div>
+                      <div className="base-stat defending">
+                        <span className="stat-box">
+                          {this.state.player.stats.goalkeeping.reflexes}
+                        </span>{" "}
+                        REF
+                      </div>
+                    </div>
+                  ) : (
+                    <div>
+                      <div className="base-stat pace">
+                        <span className="stat-box">
+                          {this.state.player.stats.generic.pace}
+                        </span>{" "}
+                        PAC
+                      </div>
+                      <div className="base-stat shooting">
+                        SHO{" "}
+                        <span className="stat-box">
+                          {this.state.player.stats.generic.shooting}
+                        </span>
+                      </div>
+                      <div className="base-stat passing">
+                        PAS{" "}
+                        <span className="stat-box">
+                          {this.state.player.stats.generic.passing}
+                        </span>
+                      </div>
+                      <div className="base-stat defending">
+                        <span className="stat-box">
+                          {this.state.player.stats.generic.defending}
+                        </span>{" "}
+                        DEF
+                      </div>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
