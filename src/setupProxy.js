93a365362594033c41544b4a6aa1d63150ae14cb
alokpurohit18 <alokpurohit18@gmail.com>
2022-03-06 12:06:18 +0530

    access point comments added

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,22 +1,28 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
+  //access point 1 and its proxy for flask server
   app.use(
     "/data_scraping",
     createProxyMiddleware({
       target: "http://127.0.0.1:5000/",
     })
   );
+
+  //access point 2 and its proxy for flask server
   app.use(
     "/player_details",
     createProxyMiddleware({
       target: "http://127.0.0.1:5000/",
     })
   );
-  app.use(
+
+  //access point 3 and its proxy for flask server
+  //currently disabled for faster server response
+  /* app.use(
     "/league_details",
     createProxyMiddleware({
       target: "http://127.0.0.1:5000/",
     })
-  );
+  ); */
 };
